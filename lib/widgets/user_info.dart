559fcd8a84d681d40428a417819dc5c1e918e06f
ABHISHEK_PATIL <abhipatil1912@gmail.com>
2021-04-04 20:12:13 +0530

    user Info added

--- lib/widgets/user_info.dart
+++ lib/widgets/user_info.dart
@@ -7,7 +7,68 @@ class UserInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(user.name),
+      margin: EdgeInsets.all(15),
+      child: Column(children: [
+        SizedBox(
+          height: 7,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              "Followers:${user.followers}",
+              style: TextStyle(fontWeight: FontWeight.w700),
+            ),
+            Text(
+              "Following:${user.following}",
+              style: TextStyle(fontWeight: FontWeight.w700),
+            ),
+          ],
+        ),
+        SizedBox(height: 10),
+        Container(
+          padding: EdgeInsets.all(15),
+          height: 150,
+          width: double.infinity,
+          child: SingleChildScrollView(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Text("Id: "),
+                  Text("Name: "),
+                  Text("Email: "),
+                  Text("Bio: "),
+                  Text("Blog: "),
+                  Text("Company: "),
+                  Text("Location: "),
+                  Text("PublicGits: "),
+                  Text("PublicRepos: "),
+                  Text("TwitterUserName: "),
+                  Text("Hireable: "),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(user.id.toString()),
+                  Text(user.name.toString()),
+                  Text(user.email.toString()),
+                  Text(user.bio.toString()),
+                  Text(user.blog.toString()),
+                  Text(user.company.toString()),
+                  Text(user.location.toString()),
+                  Text(user.publicGists.toString()),
+                  Text(user.publicRepos.toString()),
+                  Text(user.twitterUsername.toString()),
+                  Text(user.hireable.toString()),
+                ],
+              ),
+            ],
+          )),
+          decoration: BoxDecoration(border: Border.all()),
+        )
+      ]),
     );
   }
 }
