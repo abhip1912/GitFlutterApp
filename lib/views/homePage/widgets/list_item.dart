989b162272c503cfceb094cfd27ff32ea19613aa
ABHISHEK_PATIL <abhipatil1912@gmail.com>
2021-04-10 19:23:05 +0530

    Fixed issue of Note icon in searching

--- lib/views/homePage/widgets/list_item.dart
+++ lib/views/homePage/widgets/list_item.dart
@@ -3,49 +3,35 @@ import 'package:git_users/models/users.dart';
 import 'package:git_users/views/userDetailsPage/user_detail.dart';
 import 'package:provider/provider.dart';
 
-class ListItem extends StatefulWidget {
+class ListItem extends StatelessWidget {
   final Users user;
   ListItem(this.user);
-
-  @override
-  _ListItemState createState() => _ListItemState();
-}
-
-class _ListItemState extends State<ListItem> {
-  var userNote = '';
-
-  @override
-  void didChangeDependencies() async {
-    super.didChangeDependencies();
-    final String str =
-        await Provider.of<Users>(context).userNote(widget.user.id);
-    setState(() {
-      userNote = str;
-    });
-  }
-
-  bool isUserNoteEmpty(String str) {
-    if (str.length > 0) {
-      return true;
-    }
-    return false;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(left: 10, right: 10),
       child: InkWell(
         onTap: () => Navigator.of(context)
-            .pushNamed(UserDetail.routeName, arguments: widget.user),
+            .pushNamed(UserDetail.routeName, arguments: user),
         child: Card(
           child: ListTile(
-            trailing: userNote.length > 0 ? Icon(Icons.note) : Icon(null),
-            title: Text(widget.user.login),
-            subtitle: Text("${widget.user.id}-${widget.user.type}"),
+            trailing: FutureBuilder(
+              future: Provider.of<Users>(context).userNote(user.id),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Icon(null);
+                } else {
+                  return snapshot.data.length > 0
+                      ? Icon(Icons.note)
+                      : Icon(null);
+                }
+              },
+            ),
+            title: Text(user.login),
+            subtitle: Text("${user.id}-${user.type}"),
             leading: CircleAvatar(
               radius: 30,
-              backgroundImage: NetworkImage(widget.user.avatarUrl),
+              backgroundImage: NetworkImage(user.avatarUrl),
             ),
           ),
         ),
